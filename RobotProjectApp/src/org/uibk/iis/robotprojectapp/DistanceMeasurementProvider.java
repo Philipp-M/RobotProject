c727cb6a30c31605e9b8e616c0c0fb589f0d077c
Philipp Mildenberger <philipp.mildenberger@student.uibk.ac.at>
2015-03-27 21:44:15 +0100

    refactured the DistanceMeasurementProvider to accept multiple Listeners(useful for the different tabs, that maybe use in near future own odometry

--- RobotProjectApp/src/org/uibk/iis/robotprojectapp/DistanceMeasurementProvider.java
+++ RobotProjectApp/src/org/uibk/iis/robotprojectapp/DistanceMeasurementProvider.java
@@ -1,6 +1,7 @@
 package org.uibk.iis.robotprojectapp;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -30,41 +31,63 @@ public class DistanceMeasurementProvider {
 		void onDistanceBelowThreshold(short left, short center, short right);
 	}
 
-	private ChangeEventListener changeEventListener;
+	/**
+	 * just a structure that holds every necessary information that a listener
+	 * requires...
+	 */
+	private static class ChangeEventListenerContainer {
+		public ChangeEventListener changeEventListener;
+		public short minDiffForEvent;
+		public short threshold;
+		public boolean belowThreshold;
+		private short lastLeftSensorValue;
+		private short lastRightSensorValue;
+		private short lastCenterSensorValue;
+
+		public ChangeEventListenerContainer(ChangeEventListener changeEventListener, short minDiffForEvent, short threshold) {
+			this.changeEventListener = changeEventListener;
+			this.minDiffForEvent = minDiffForEvent;
+			this.threshold = threshold;
+			this.belowThreshold = false;
+		}
+	}
+
+	private List<ChangeEventListenerContainer> changeEventListeners;
 	private Timer measurementTimer;
 	private short leftSensorValue;
-	private short lastLeftSensorValue;
 	private short rightSensorValue;
-	private short lastRightSensorValue;
 	private short centerSensorValue;
-	private short lastCenterSensorValue;
-	private short minDiffForEvent;
 	private long deltaTime;
 	private short threshold;
-	private boolean belowThreshold;
 
+	private static final class InstanceHolder {
+		static final DistanceMeasurementProvider INSTANCE = new DistanceMeasurementProvider();
+	}
+
+	private DistanceMeasurementProvider() {
+		changeEventListeners = new ArrayList<DistanceMeasurementProvider.ChangeEventListenerContainer>();
+	}
+
+	public static DistanceMeasurementProvider getInstance() {
+		return InstanceHolder.INSTANCE;
+	}
+
+	// this method has to be called first otherwise the other (listener) methods
+	// won't work
 	/**
-	 * @param minDiffForEvent
-	 *            minimum difference('delta'-length) between two measurements to
-	 *            trigger an event to the listener
 	 * @param deltaTime
 	 *            time between measurements has to be bigger than 115ms because
 	 *            the robot is to slow to answer(or to be exact it just sends
-	 *            quite much overhead(String instead of binary) a fix is already
-	 *            available which reduces this delay to 35ms(BINARY_READING))
-	 * @param threshold
-	 *            below this value a belowThreshold event is sent, used for
-	 *            example as stopping detection
+	 *            quite much overhead(string instead of binary). A fix which has
+	 *            to be flashed on the robot interface is already available
+	 *            which reduces this delay to 35ms(BINARY_READING))
 	 */
-	public DistanceMeasurementProvider(short minDiffForEvent, long deltaTime, short threshold) {
-		this.minDiffForEvent = minDiffForEvent;
+	public void init(long deltaTime) {
 		this.deltaTime = deltaTime;
-		this.threshold = threshold;
-		belowThreshold = false;
 	}
 
 	/**
-	 * starts the measurements
+	 * starts the measurements and listener events,
 	 */
 	public void start() {
 		measurementTimer = new Timer();
@@ -72,10 +95,38 @@ public class DistanceMeasurementProvider {
 	}
 
 	/**
-	 * stops the measurements
+	 * stops the measurements and listener events
 	 */
 	public void stop() {
 		measurementTimer.cancel();
+	}
+
+	/**
+	 * registers a listener
+	 * 
+	 * @param changeEventListener
+	 *            the listener to register
+	 * @param threshold
+	 *            below this value a belowThreshold event is sent, used for
+	 *            example as stopping detection
+	 * @param minDiffForEvent
+	 *            minimum difference('delta'-length) between two measurements to
+	 *            trigger an event to the listener
+	 */
+	public void registerListener(ChangeEventListener changeEventListener, short minDiffForEvent, short threshold) {
+		changeEventListeners.add(new ChangeEventListenerContainer(changeEventListener, minDiffForEvent, threshold));
+	}
+
+	/**
+	 * unregisters the given listener
+	 * 
+	 * @param changeEventListener
+	 */
+	public void unregisterListener(ChangeEventListener changeEventListener) {
+		for (ChangeEventListenerContainer changeEventListenerContainer : changeEventListeners) {
+			if (changeEventListenerContainer.changeEventListener == changeEventListener)
+				changeEventListeners.remove(changeEventListenerContainer);
+		}
 	}
 
 	/**
@@ -90,16 +141,16 @@ public class DistanceMeasurementProvider {
 	 *             thrown if the string is of incorrect syntax
 	 */
 	private void sensorStringParser(String sensorString) throws IllegalArgumentException, NumberFormatException {
-		String delims = "[ ]+";
+		String delims = "\\s+";
 		String[] tokens = sensorString.split(delims);
-		if (tokens.length != 9)
+		if (tokens.length != 12)
 			throw new IllegalArgumentException("Error while parsing string, the number of sensors is not as expected: " + tokens.length);
-		if (tokens[0].compareTo("sensor:") != 0)
+		if (tokens[3].compareTo("sensor:") != 0)
 			throw new IllegalArgumentException("Error while parsing string, the 'magic' bytes at the beginning do not match 'sensor:' : "
-					+ tokens[0]);
-		leftSensorValue = Short.decode(tokens[LEFT_SENSOR + 1]);
-		rightSensorValue = Short.decode(tokens[RIGHT_SENSOR + 1]);
-		centerSensorValue = Short.decode(tokens[CENTER_SENSOR + 1]);
+					+ tokens[3]);
+		leftSensorValue = Short.decode(tokens[LEFT_SENSOR + 4]);
+		rightSensorValue = Short.decode(tokens[RIGHT_SENSOR + 4]);
+		centerSensorValue = Short.decode(tokens[CENTER_SENSOR + 4]);
 	}
 
 	public short getLeftSensorValue() {
@@ -114,34 +165,44 @@ public class DistanceMeasurementProvider {
 		return centerSensorValue;
 	}
 
+	/**
+	 * The timer task that updates the measurements and sends events to the
+	 * listeners
+	 */
 	private class MeasurementTimerTask extends TimerTask {
 		@Override
 		public void run() {
 			if (!BINARY_READING) {
+				try {
 				sensorStringParser(ComDriver.getInstance().comReadWrite(new byte[] { 'q', '\r', '\n' }));
+				} catch(IllegalArgumentException e) {
+					e.printStackTrace();
+				}
 			} else {
 				ArrayList<Byte> sensorData = ComDriver.getInstance().comReadBinWrite(new byte[] { 'p', '\r', '\b' });
 				leftSensorValue = sensorData.get(LEFT_SENSOR);
 				rightSensorValue = sensorData.get(RIGHT_SENSOR);
 				centerSensorValue = sensorData.get(CENTER_SENSOR);
 			}
-			if (Math.abs(lastLeftSensorValue - leftSensorValue) >= minDiffForEvent
-					|| Math.abs(lastRightSensorValue - rightSensorValue) >= minDiffForEvent
-					|| Math.abs(lastCenterSensorValue - centerSensorValue) >= minDiffForEvent) {
-				lastLeftSensorValue = leftSensorValue;
-				lastRightSensorValue = rightSensorValue;
-				lastCenterSensorValue = centerSensorValue;
-				if (changeEventListener != null) {
-					changeEventListener.onDistanceChanged(leftSensorValue, centerSensorValue, rightSensorValue);
+			for (ChangeEventListenerContainer cEListenCont : changeEventListeners) {
+				if (Math.abs(cEListenCont.lastLeftSensorValue - leftSensorValue) >= cEListenCont.minDiffForEvent
+						|| Math.abs(cEListenCont.lastRightSensorValue - rightSensorValue) >= cEListenCont.minDiffForEvent
+						|| Math.abs(cEListenCont.lastCenterSensorValue - centerSensorValue) >= cEListenCont.minDiffForEvent) {
+					cEListenCont.lastLeftSensorValue = leftSensorValue;
+					cEListenCont.lastRightSensorValue = rightSensorValue;
+					cEListenCont.lastCenterSensorValue = centerSensorValue;
+					cEListenCont.changeEventListener.onDistanceChanged(leftSensorValue, centerSensorValue, rightSensorValue);
 				}
+
+				if (!cEListenCont.belowThreshold
+						&& (leftSensorValue <= cEListenCont.threshold || rightSensorValue <= cEListenCont.threshold || centerSensorValue <= cEListenCont.threshold)) {
+					cEListenCont.belowThreshold = true;
+					cEListenCont.changeEventListener.onDistanceBelowThreshold(leftSensorValue, centerSensorValue, rightSensorValue);
+				}
+
+				if (leftSensorValue > threshold && rightSensorValue > cEListenCont.threshold && centerSensorValue <= cEListenCont.threshold)
+					cEListenCont.belowThreshold = false;
 			}
-			if (changeEventListener != null && !belowThreshold
-					&& (leftSensorValue <= threshold || rightSensorValue <= threshold || centerSensorValue <= threshold)) {
-				belowThreshold = true;
-				changeEventListener.onDistanceBelowThreshold(leftSensorValue, centerSensorValue, rightSensorValue);
-			}
-			if ((leftSensorValue > threshold && rightSensorValue > threshold && centerSensorValue <= threshold))
-				belowThreshold = false;
 		}
 	}
 }
