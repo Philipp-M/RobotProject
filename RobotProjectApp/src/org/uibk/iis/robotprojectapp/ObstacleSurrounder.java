8c1a0c556c5830c062af2e3ed5108e8656c95634
Philipp Mildenberger <philipp.mildenberger@student.uibk.ac.at>
2015-04-17 15:28:42 +0200

    added ObstacleSurrounder Base Class

---
+++ RobotProjectApp/src/org/uibk/iis/robotprojectapp/ObstacleSurrounder.java
@@ -0,0 +1,21 @@
+package org.uibk.iis.robotprojectapp;
+
+public class ObstacleSurrounder {
+
+	public interface PositionListener {
+		void onPositionUpdated(OdometryManager.Position pos);
+
+	}
+
+	public enum Dir {
+		LEFT, RIGHT
+	}
+	private Dir direction;
+	private int distanceToObject;
+	
+	public ObstacleSurrounder(Dir direction, int distanceToObject) {
+		this.direction = direction;
+		this.distanceToObject = distanceToObject;
+	}
+	
+}
